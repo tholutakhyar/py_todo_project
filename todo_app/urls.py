ae9df3d8ee72a9a73e6510a17dbcc919305d1c74
Tholut Akhyar <tholutx@gmail.com>
2023-11-26 16:59:07 +0700

    Initial commit

---
+++ todo_app/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from .views import TodoViewSet
+
+router = DefaultRouter()
+router.register(r'todos', TodoViewSet, basename='todo')
+
+urlpatterns = router.urls
